45d524e4133bff2e31ceca3e00c340ac66aca81e
neil patel <neil@myindianweddingapp.com>
2019-03-20 19:44:02 +0000

    Added mouse move event listener on slideshow section

---
+++ hover.js
@@ -0,0 +1,6 @@
+const slideshow = document.querySelector('section.slideshow')
+const images = slideshow.querySelectorAll('img')
+
+slideshow.addEventListener('mousemove', function (event) {
+    console.log(event)
+})
